878e36052c6cb74e82dfb85e0e770283114313cf
Carlos Clayton <carlos.clayton@gmail.com>
2020-08-10 19:33:11 -0300

    Updating composer

---
+++ app/Http/Controllers/FacebookController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Cache\LaravelCache;
+use BotMan\BotMan\Drivers\DriverManager;
+use BotMan\Drivers\Facebook\FacebookDriver;
+use Illuminate\Support\Collection;
+
+class FacebookController extends Controller
+{
+    /**
+     * Place your BotMan logic here.
+     */
+    public function handle()
+    {
+//        $botman = app('botman');
+
+        $config = [
+            "web" => [
+                "matchingData" => [
+                    "driver" => "facebook",
+                ]
+            ],
+            "botman" => [
+                "conversation_cache_time" => 3600,
+                "user_cache_time" => 3600,
+            ],
+            "facebook" => [
+                "token" => "EAADrUyTBRMUBALrY6F3ZCLMvI8bmSPM90EOf3ICZAzpeD4NCbqZB54TLKvWvasdAY7Bhi45s8KQYSQSCFdzxUFI1LkW84yYzl5TGHMGsn4FQKbz0P0eVKg8hqlAJZBQUxeDlEW6bnvi9dioj4ummLwHtn5rTmK6WsYQZAzExmZA4Gwfd32twQUp2WjQr34KKMZD",
+            ]
+
+        ];
+
+        DriverManager::loadDriver(FacebookDriver::class);
+        $botman = BotManFactory::create($config, new LaravelCache());
+
+
+        $botman->fallback(function ($bot) {
+            $bot->typesAndWaits(1);
+            $bot->reply($this->fallbackResponse());
+        });
+
+
+        $botman->listen();
+
+
+    }
+
+    public function fallbackResponse()
+    {
+        return Collection::make([
+            'Desculpe, não entendi. Poderia repetir, por favor?',
+            'Ainda não compreendi, poderia tentar novamente?',
+            'Opa! não consegui entender, poderia repetir',
+            'Ok, vamos começar de novo. Poderia tentar mais uma vez?'
+        ])->random();
+    }
+
+
+}
